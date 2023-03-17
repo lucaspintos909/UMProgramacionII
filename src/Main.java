1e55f53d13c11a82be17eef37ee09ff5398709eb
lucaspintos909 <lucaspintos909@gmail.com>
2023-03-17 11:38:39 -0300

    Pruebas con LinkedLists

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import utils.classes.LinkedList;
+import utils.classes.Node;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        LinkedList lista = new LinkedList();
+        lista.add(10);
+        lista.add(20);
+        lista.add(30);
+        lista.add(40);
+        lista.addFirst(5);
+        lista.addLast(50);
+
+        Node n = lista.getHead();
+
+        while (n.next != null) {
+            System.out.println(n.value);
+            n = n.next;
+        }
+        System.out.println(n.value);
+        System.out.println("Existe el 1: " +  lista.exists(1));
+
     }
 }
