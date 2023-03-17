496e20861bec91818a0afe8b70240df9991db964
lucaspintos909 <lucaspintos909@gmail.com>
2023-03-17 12:05:14 -0300

    Moviendo pruebas de linked lists a su archivo especifico

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,6 @@
-import TADs.LinkedList.LinkedList;
-import TADs.LinkedList.Node;
 
 public class Main {
     public static void main(String[] args) {
-        LinkedList lista = new LinkedList();
-        lista.add(10);
-        lista.add(20);
-        lista.add(30);
-        lista.add(40);
-        lista.addFirst(5);
-        lista.addLast(50);
-
-        Node n = lista.getHead();
-
-        while (n.next != null) {
-            System.out.println(n.value);
-            n = n.next;
-        }
-        System.out.println(n.value);
-        System.out.println("Existe el 1: " + lista.exists(1));
-
+        System.out.println("Hello World!");
     }
 }
