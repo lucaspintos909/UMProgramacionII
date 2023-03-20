7023626ebd7ca53dc4d047e7fbf1a0146ea3cdef
lucaspintos909 <lucaspintos909@gmail.com>
2023-03-20 20:11:22 -0300

    Agregando pruebas para lista doblemente enlazada

--- src/practicos/practico1/Ejercicio1.java
+++ src/practicos/practico1/Ejercicio1.java
@@ -11,19 +11,27 @@ public class Ejercicio1 {
         lista.add(20);
         lista.add(30);
         lista.add(40);
-        lista.addFirst(5);
-        lista.addLast(50);
+        /*lista.addFirst(5);
+        lista.addLast(50);*/
 
-        lista.remove(2);
+        /*lista.remove(2);*/
         Object valorPosicion1 = lista.get(1);
 
         Node n = lista.getHead();
+        Node prev;
 
         while (n.next != null) {
-            System.out.println(n.value);
+            prev = n.prev;
+            System.out.println("Valor: " + n.value);
+            if (n.prev != null) {
+                System.out.println("Valor previo de " + n.value + ": " + prev.value);
+            }
             n = n.next;
         }
-        System.out.println(n.value);
+        prev = n.prev;
+        System.out.println("Valor: " + n.value);
+        System.out.println("Valor previo de " + n.value + ": " + prev.value);
+
         System.out.println("Existe el 1: " + lista.exists(1));
         System.out.println("Obteniendo valor de la posicion 1: " + valorPosicion1);
     }
