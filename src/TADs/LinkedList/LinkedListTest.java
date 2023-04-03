959d1f6a5e45ee41088588d4c1567b143ae331c1
lucaspintos909 <lucaspintos909@gmail.com>
2023-04-03 11:33:20 -0300

    Agregando tests unitarios a LinkedList

---
+++ src/TADs/LinkedList/LinkedListTest.java
@@ -0,0 +1,32 @@
+package TADs.LinkedList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class LinkedListTest {
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void get() {
+    }
+
+    @Test
+    void addFirst() {
+    }
+
+    @Test
+    void addLast() {
+    }
+
+    @Test
+    void exists() {
+    }
+}
