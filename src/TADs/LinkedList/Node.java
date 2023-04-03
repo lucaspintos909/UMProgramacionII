d4be8d39c7fa5d59d011757bd19d3e52206cae55
lucaspintos909 <lucaspintos909@gmail.com>
2023-04-03 12:09:29 -0300

    Agregando getters a Node

--- src/TADs/LinkedList/Node.java
+++ src/TADs/LinkedList/Node.java
@@ -1,11 +1,23 @@
 package TADs.LinkedList;
 
-class Node<T> {
+public class Node<T> {
     T value;
     Node<T> next;
     Node<T> prev;
 
     public Node(T value) {
         this.value = value;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public Node<T> getNext() {
+        return next;
+    }
+
+    public Node<T> getPrev() {
+        return prev;
     }
 }
