ee13ff19eaa4d02c4fd1d1c8dae50e43f2b80c39
lucaspintos909 <lucaspintos909@gmail.com>
2023-04-12 12:30:49 -0300

    Sacando nodo next en Node, innecesario

--- src/TADs/Stack/Node.java
+++ src/TADs/Stack/Node.java
@@ -2,21 +2,15 @@ package TADs.Stack;
 
 public class Node<T> {
     T value;
-    Node<T> next;
     Node<T> prev;
 
     public Node(T value) {
         this.value = value;
-        this.next = null;
         this.prev = null;
     }
 
     public T getValue() {
         return value;
-    }
-
-    public Node<T> getNext() {
-        return next;
     }
 
     public Node<T> getPrev() {
